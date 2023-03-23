509da53bc7cfac5a3001a203ff6c84fd78e905a7
Santiago Pardo <s.pardob@uniandes.edu.co>
2023-03-23 14:47:33 -0500

    first front

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException,Request,Form, Cookie
+from fastapi.responses import HTMLResponse , RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 from sqlalchemy.orm import Session
 
 from models import user as userModel
@@ -15,4 +19,23 @@ app = FastAPI()
 
 app.include_router(user)
 app.include_router(item)
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static",StaticFiles(directory="static"),name="static")
+
+@app.get("/index",response_class=HTMLResponse)
+def get_index(request:Request):
+    return templates.TemplateResponse("index.html", {"request":request})
+
+@app.post("/index", response_class=HTMLResponse)
+def form_post(request: Request, user: str = Form(...), password: str = Form(...)):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/login",response_class=HTMLResponse)
+def get_login(request:Request):
+    return templates.TemplateResponse("login.html", {"request":request})
+
+@app.post("/login",response_class=HTMLResponse)
+def post_login(request:Request):
+    return templates.TemplateResponse("login.html", {"request":request})
 
